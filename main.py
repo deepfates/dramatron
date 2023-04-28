9533460608f0fca45c4c4cb77ce8d8ccef5b741b
max brewer <deepfates@gmail.com>
2023-04-28 16:38:54 -0600

    Add user interaction

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from rich import print
 from bot import Bot
 
 class Channel:
-    def __init__(self, time_limit = 300):
+    def __init__(self, time_limit = 15):
         self.closed = False
         self.time_limit = time_limit
         self.conversation = ["Narrator: Let the scene begin!"]
@@ -15,12 +15,24 @@ class Channel:
 
     def get_history(self):
         return self.conversation[-5:]
+    
+    def get_user_input(self):
+        print("[blue]Narrator[/blue]: ", end="")
+        text = input()
+        self.start = time.time()
+        if len(text) == 0:
+            return
+        if text in ["quit", "exit", "q", "e"]:
+            self.closed = True
+            return
+        formatted_input = f"Narrator: {text}"
+        self.conversation.append(formatted_input)
 
     def add_message(self, message):
         self.conversation.append(message)
         print(message)
         if time.time() - self.start > self.time_limit:
-            self.closed = True
+            self.get_user_input()
 
 if __name__ == "__main__":  
     from prompts import bots
